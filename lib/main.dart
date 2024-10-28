96f63bbf4f0715d83f7ce92f311631f2eb20d446
Santosh Dahal <sirishsds@gmail.com>
2024-10-28 10:22:04 +0545

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
-}
+} //
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
